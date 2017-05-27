37f3bafba137d1036ea2556c9be180fed2e8c675
Robert Corrêa dos Santos <robertcorreas@gmail.com>
2017-05-27 10:33:34 -0300

    inclusão de testes com xunit

---
+++ WpfApp1/Testes/PontoTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WpfApp1.Testes
+{
+    public class PontoTest
+    {
+        [Fact]
+        public void DevePossuirXY()
+        {
+            var p = new Ponto(10,20);
+
+            Assert.Equal(10, p.X);
+            Assert.Equal(20, p.Y);
+        }
+
+    }
+}
